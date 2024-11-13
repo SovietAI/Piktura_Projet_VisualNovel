340eac41b73efdfa3928a51b21bbd0224197bfd8
Yann-Warlop <yann.warlop@outlook.fr>
2024-11-13 12:42:47 +0100

    Initial Commit
    Proto Branch First Commit

---
+++ Assets/Scripts/WindowSystem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Serialization;
+
+public class WindowSystem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private Vector3 _initialPosition; // Position initiale de la boite    
+    void Start()
+    {
+        //Guh
+    }
+
+    public void OnDrag(PointerEventData eventData) // Fonction de Drag
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x + eventData.delta.x, transform.localPosition.y + + eventData.delta.y);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _initialPosition = transform.localPosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //transform.localPosition = _initialPosition; //SnapBack
+    }
+}
